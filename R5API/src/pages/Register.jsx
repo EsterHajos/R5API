57b64a415b9d13a16cad58f78f7b0e864dcd70d3
EsterHajos <ester.marta.hajos@gmail.com>
2026-06-08 10:33:37 +0200

    added code for registration function

--- R5API/src/pages/Register.jsx
+++ R5API/src/pages/Register.jsx
@@ -3,7 +3,20 @@ import { useNavigate} from "react-router-dom";
 
 function Register () {
 
-    
+    const [username, setUsername] = useState (" ");
+    const [password, setPassword] = useState (" ");
+
+    const navigate = useNavigate();
+
+    const handleRegister = () => {
+        if (password.lenght < 5) {
+            alert("Password must be at least 5 characters");
+            return;
+        }
+
+
+
+    }
 
 
 
