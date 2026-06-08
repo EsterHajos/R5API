04d103f27ae9cde9cc0e8ddbd633ebd8e1d07099
EsterHajos <ester.marta.hajos@gmail.com>
2026-06-08 11:27:38 +0200

    page empty trying to fix

--- R5API/src/pages/Register.jsx
+++ R5API/src/pages/Register.jsx
@@ -3,13 +3,13 @@ import { useNavigate} from "react-router-dom";
 
 function Register () {
 
-    const [username, setUsername] = useState (" ");
-    const [password, setPassword] = useState (" ");
+    const [username, setUsername] = useState ("");
+    const [password, setPassword] = useState ("");
 
     const navigate = useNavigate();
 
     const handleRegister = () => {
-        if (password.lenght < 5) {
+        if (password.length < 5) {
             alert("Password must be at least 5 characters");
             return;
         }
