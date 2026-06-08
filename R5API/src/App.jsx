c8f4c4b47b7f9d481b76c27ba7b1fb96d6f0df1b
EsterHajos <ester.marta.hajos@gmail.com>
2026-06-08 11:06:28 +0200

    changing app.jsx - routes

--- R5API/src/App.jsx
+++ R5API/src/App.jsx
@@ -1,11 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 import Game from "./components/Game";
 
 function App() {
-  return (
-    <div>
-      <Game />
-    </div>
-  );
-}
 
-export default App;
+return (
+  <BrowserRouter>
+  <Routes>
+  <Route path="/" element={<Register />} />
+  <Route path="/login" element={<Login />} />
+  <Route path="/game" element={<Game />} />
+  </Routes>
+  
+  
+  </BrowserRouter>
+
+
+)
+
+
+
+
+}
