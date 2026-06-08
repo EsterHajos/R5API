5503d4489fa31a05c07a1ff753277393080ba569
EsterHajos <ester.marta.hajos@gmail.com>
2026-06-08 10:40:29 +0200

    added input code and bútton for register

--- R5API/src/pages/Register.jsx
+++ R5API/src/pages/Register.jsx
@@ -14,10 +14,24 @@ function Register () {
             return;
         }
 
+        localStorage.setItem("username", username);
+        localStorage.setItem("password", password);
+        alert ("Account created!"); 
 
+        navigate ("/login");
+    };
 
-    }
+    return (
+        <div>
+            <h1>Register to play!</h1>
 
+            <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername (e.target.value)}/>
+            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
 
-
+            <button onClick={handleRegister}>Register</button>
+        </div>
+    );
 }
+
+
+export default Register;
